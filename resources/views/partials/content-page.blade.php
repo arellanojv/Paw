106bc1b78e70ad3d6dce871405164a1e138c89d9
Jhon Arellano <jah4n@Jhons-MacBook-Air.local>
2024-08-29 10:26:49 +0800

    Added content area and footer section

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -2,6 +2,45 @@
  @include('partials.content-hero')
 @endif
 
+@if (! is_front_page()) 
+  <!--..:: Breadcrumb Section Start ::.. -->
+  <section class="section-breadcrumb">
+      <!-- Breadcrumb Section Spacer -->
+      <div class="breadcrumb-wrapper">
+          <!-- Section Container -->
+          <div class="container-default">
+              <div class="breadcrumb-block">
+                  <h1 class="breadcrumb-title">About Us</h1>
+                  <ul class="breadcrumb-nav">
+                      <li><a href="index.php">Home</a></li>
+                      <li>About</li>
+                  </ul>
+              </div>
+          </div>
+          <!-- Section Container -->
+
+          <!-- Breadcrumb Shape - 1 -->
+          <div class="absolute left-0 top-0 -z-10">
+              <img src="@asset('images/elements/breadcrumb-shape-1.svg')" alt="hero-shape-1" width="291" height="380" />
+          </div>
+
+          <!-- Breadcrumb Shape - 2 -->
+          <div class="absolute bottom-0 right-0 -z-[1]">
+              <img src="@asset('images/elements/breadcrumb-shape-2.svg')" alt="hero-shape-2" width="291" height="380" />
+          </div>
+      </div>
+      <!-- Breadcrumb Section Spacer -->
+  </section>
+@endif
+
+<div class="section-space">
+  <div class="container-custom has-container-custom">
+    <div class="grid gap-10 gap-x-12">
+      @php(the_content())
+    </div>
+  </div>
+</div>
+
 @if ($pagination)
   <nav class="page-nav" aria-label="Page">
     {!! $pagination !!}

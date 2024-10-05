e27951afbd219bfad0d51c514d22359b65aa3218
Jhon Arellano <jah4n@Jhons-MacBook-Air.local>
2024-10-05 17:30:50 +0800

    Location taxonomy list and template

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -34,33 +34,57 @@
 @endif
 
 
+
+
 <div class="section-space">
     <div class="container-custom has-container-custom">
         <div class="grid gap-10 gap-x-12">
-            @php(the_content())
+
+
+
+
+            {{-- Display the regular post/page content --}}
+            <?php the_content(); ?>
+
+            @php
+                // Define the parent term slug (e.g., 'cebu' as the parent location)
+                $parent_location = get_term_by('slug', 'cebu', 'location');
+
+                // Fetch child locations for the specified parent location
+                $child_locations = get_terms([
+                    'taxonomy' => 'location', // The taxonomy name
+                    'parent' => $parent_location->term_id, // Parent location ID
+                    'hide_empty' => false, // Show even if there are no listings
+                ]);
+            @endphp
+            <div
+                class="grid justify-items-center md:grid-cols-4 sm:grid-cols-2 min-[320px]:grid-cols-2 gap-4 bg-ColorDarkBlue text-white p-10 rounded-xl lg:mx-20 z-10">
+                {{-- Loop through and display child locations --}}
+                @if (!empty($child_locations) && !is_wp_error($child_locations))
+
+                    @foreach ($child_locations as $child_location)
+                        <div>
+                            <a href="{{ get_term_link($child_location) }}">
+                                {{ $child_location->name }}
+                            </a>
+                        </div>
+                    @endforeach
+                @else
+                    <p>No sub locations found.</p>
+                @endif
+
+
+            </div>
+
+
         </div>
 
 
-        <div
-            class="grid justify-items-center md:grid-cols-4 sm:grid-cols-2 min-[320px]:grid-cols-2 gap-4 bg-ColorDarkBlue text-white p-10 rounded-xl lg:mx-20 z-10">
-            <div><a href="#">Argao</a></div>
-            <div>Asturias</div>
-            <div>Balamban</div>
-            <div>Carmen</div>
-            <div>Cebu City</div>
-            <div>Compostela</div>
-            <div>Consolacion</div>
-            <div>Cordova</div>
-            <div>Danao City</div>
-            <div>Lapu-Lapu City</div>
-            <div>Liloan</div>
-            <div>Mandaue City</div>
-            <div>Minglanilla</div>
-            <div>Naga</div>
-            <div>Oslob</div>
-            <div>Talisay City</div>
-            <div>Toledo City</div>
-        </div>
+
+
+
+
+
     </div>
 </div>
 

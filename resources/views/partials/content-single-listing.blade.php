8bfc7c2e813945d44fccdf2b7732492c71d74ada
Jhon Arellano <jah4n@Jhons-MacBook-Air.local>
2024-10-07 10:40:52 +0800

    location listing template and listing single template

---
+++ resources/views/partials/content-single-listing.blade.php
@@ -0,0 +1,61 @@
+<!--..:: Blog Section Start ::.. -->
+<div class="blog-section">
+    <!-- Section Spacer -->
+    <div class="section-space">
+        <!-- Section Container -->
+        <div class="container-default">
+            <div class="grid grid-cols-1 gap-x-6 gap-y-10 lg:grid-cols-[minmax(416px,_0.45fr),1fr]">
+                <!-- Blog Aside -->
+                <aside class="jos flex flex-col gap-y-[30px]">
+                    <!-- Single Sidebar -->
+                    <div class="rounded-[10px] bg-ColorOffWhite p-8">
+                        <div
+                            class="relative mb-[30px] inline-block pb-[10px] text-lg font-semibold text-ColorBlack after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:bg-black">
+                            Blog Categories
+                        </div>
+                        <!-- Blog Categories List -->
+                        <ul class="text-ColorBlack">
+                            <li
+                                class="border-b border-ColorBlack/10 pb-[14px] pt-[14px] first:pt-0 last:border-b-0 last:pb-0">
+                                <a href="blog-details.php" class="hover:text-ColorBlue">Business & Marketing (18)</a>
+                            </li>
+                            <li
+                                class="border-b border-ColorBlack/10 pb-[14px] pt-[14px] first:pt-0 last:border-b-0 last:pb-0">
+                                <a href="blog-details.php" class="hover:text-ColorBlue">Technology (05)</a>
+                            </li>
+                            <li
+                                class="border-b border-ColorBlack/10 pb-[14px] pt-[14px] first:pt-0 last:border-b-0 last:pb-0">
+                                <a href="blog-details.php" class="hover:text-ColorBlue">Art & Beauty (23)</a>
+                            </li>
+                            <li
+                                class="border-b border-ColorBlack/10 pb-[14px] pt-[14px] first:pt-0 last:border-b-0 last:pb-0">
+                                <a href="blog-details.php" class="hover:text-ColorBlue">Digital Agency (10)</a>
+                            </li>
+                        </ul>
+                        <!-- Blog Categories List -->
+                    </div>
+                    <!-- Single Sidebar -->
+                </aside>
+                <!-- Blog Aside -->
+
+                <div class="flex flex-col gap-y-10 lg:gap-y-14 xl:gap-y-20">
+                    <!-- Blog Post Area -->
+                    <div class="flex flex-col gap-6">
+                        <!-- Blog Post Article -->
+
+                        {!! $title !!}
+
+                        <!-- Horizontal Separator -->
+                        <div class="jos my-[30px] h-[1px] w-full bg-[#EAEDF0]"></div>
+                        <!-- Horizontal Separator -->
+
+                    </div>
+                    <!-- Blog Post Detail Area -->
+                </div>
+            </div>
+        </div>
+        <!-- Section Container -->
+    </div>
+    <!-- Section Spacer -->
+</div>
+<!--..:: Blog Section End ::.. -->
